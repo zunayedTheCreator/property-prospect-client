969f035e5b42c951c3c3257bf058beb30b1793d9
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-01 16:02:30 +0600

    extra section added

---
+++ src/Pages/Home/Ad/Ad.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ad = () => {
+    return (
+        <div className='bg-[#17242A] h-[150px] lg:h-[300px] flex items-center justify-center mb-14 mt-4'>
+            <h3 className='text-xl md:text-3xl lg:text-6xl font-bold text-[#FEFFFF]'>- Find the home you want. -</h3>
+        </div>
+    );
+};
+
+export default Ad;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Advertisement from './Advertisement/Advertisement';
 import Banner from './Banner/Banner';
 import Header from '../../Shared/Header/Header';
 import Reviews from './Reviews/Reviews';
+import Ad from './Ad/Ad';
 
 const Home = () => {
     return (
@@ -11,6 +12,9 @@ const Home = () => {
             <div className='pt-20 max-w-7xl mx-auto'>
                 <Header header='Advertisement' ></Header>
                 <Advertisement></Advertisement>
+            </div>
+            <Ad></Ad>
+            <div className='max-w-7xl mx-auto'>
                 <Header header='Latest User reviews' ></Header>
                 <Reviews></Reviews>
             </div>
