15b1fe712b4bd862bb7aa30faf16974827cb236a
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-02 20:53:49 +0600

    signIn system added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 import lock from '../../../public/images/lock.gif'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
 const Login = () => {
+
+    const {signIn} = createContext(AuthContext);
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+    } 
+
     return (
         <div className='mt-24 mb-6'>
             <div  className='max-w-sm border-2 mx-auto rounded-md border-[#FEFFFF]'>
                 <h2 className='text-[#FEFFFF] font-bold text-4xl py-3 text-center bg-[#17242A] rounded-t-md border-b-2 border-[#FEFFFF]'>Login</h2>
-                <form className='px-3 py-4 container-bottom'>
+                <form onSubmit={handleLogin} className='px-3 py-4 container-bottom'>
                     <div>
                         <input type="email" name='email' placeholder="Enter Your Email" className="input input-bordered w-full rounded-md bg-[#FEFFFF] text-[#17242A] font-bold mb-4 mt-3" />
                         <input type="password" name='password' placeholder="Your Password" className="input input-bordered w-full rounded-md bg-[#FEFFFF] text-[#17242A] font-bold mb-3" />
