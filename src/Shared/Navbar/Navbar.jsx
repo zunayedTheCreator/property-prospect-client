db8947283a6dfa1f75d9d550c563d826a01ff027
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-03 00:14:24 +0600

    navbar updated and sign out system added

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { MdDashboard } from "react-icons/md";
+import { AuthContext } from '../../providers/AuthProvider';
+import { VscSignOut } from "react-icons/vsc";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Swal from 'sweetalert2'
+import { FaEdit, FaUser } from 'react-icons/fa';
 
 const Navbar = () => {
+    const {user, logOut} = useContext(AuthContext)
+    const navigate = useNavigate();
     const navLinks = <>
         <NavLink to={'/'} className={({ isActive, isPending }) =>
                       isActive
@@ -19,8 +27,26 @@ const Navbar = () => {
                         : "rounded-full font-bold text-[#FEFFFF]"
                     }><li><a>All Properties</a></li></NavLink>
     </>
+
+    const handleSignOut = () => {
+        logOut()
+        .then(() => {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Successfully Logged Out!",
+                showConfirmButton: false,
+                timer: 2000
+            });
+            navigate('/');
+        })
+        .catch(error => {
+            toast.error(error)
+        })
+    }
+
     return (
-        <div className="navbar bg-[#17242A] text-[#FEFFFF] px-6 z-[1000] top-0 left-0 fixed">
+        <div className="navbar bg-[#17242A] text-[#FEFFFF] px-2 md:px-6 z-[1000] top-0 left-0 fixed">
             <div className="navbar-start">
                 <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -38,10 +64,42 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="navbar-end">
-                <h3 className='text-sm font-bold border-r-2 border-[#FEFFFF] pr-2 mr-2'>Dashboard</h3>
-                <NavLink to={'/dashboard'}><MdDashboard className='text-2xl mr-4 hover:rotate-12 duration-200'></MdDashboard></NavLink>
-                <NavLink to={'/login'}><a className="btn min-h-0 h-8 bg-transparent hover:bg-[#DEF2F1] font-bold rounded px-4 border-2 border-[#FEFFFF] text-[#FEFFFF] hover:text-black hover:border-[#FEFFFF]">Login</a></NavLink>
+                <h3 className='hidden md:flex text-sm font-bold border-r-2 border-[#FEFFFF] pr-2 mr-2'>Dashboard</h3>
+                <NavLink to={'/dashboard'}><MdDashboard className='text-2xl mr-2 hover:rotate-12 duration-200 md:mr-5'></MdDashboard></NavLink>
+                {user ? <>
+                    <div className="dropdown dropdown-end border-x-2 border-[#FEFFFF] md:border-none px-2 md:px-0 mr-2">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar border-2 border-[#FEFFFF] hover:border-[#FEFFFF]">
+                            <div className="w-10 rounded-full">
+                            <img alt={user?.displayName} src={user?.photoURL} />
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#17242A] rounded-box w-52 border border-[#FEFFFF] gap-2">
+                            <h3 className='font-bold text-lg ml-2'>{user?.displayName}</h3>
+                            <div className='border-b-2 border-[#FEFFFF]'></div>
+                            <NavLink to={'/profile'} className={({ isActive, isPending }) =>
+                                isActive
+                                ? " bg-[#DEF2F1] hover:bg-[#FEFFFF] rounded-full font-bold text-black"
+                                : isPending
+                                ? "pending text-[#FEFFFF]"
+                                : "rounded-full font-bold text-[#FEFFFF]"
+                            }><li><a><FaUser></FaUser>Profile</a></li></NavLink>
+                            <NavLink to={'/update-profile'} className={({ isActive, isPending }) =>
+                                isActive
+                                ? " bg-[#DEF2F1] hover:bg-[#FEFFFF] rounded-full font-bold text-black"
+                                : isPending
+                                ? "pending text-[#FEFFFF]"
+                                : "rounded-full font-bold text-[#FEFFFF]"
+                            }><li><a><FaEdit></FaEdit>Update Profile</a></li></NavLink>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                    <h3 className='hidden md:flex font-bold border-r-2 border-[#FEFFFF] pr-2 mr-2'>{user?.displayName}</h3>
+                    <VscSignOut onClick={handleSignOut} className='text-3xl hover:-rotate-12 duration-200 cursor-pointer'></VscSignOut>
+                </> : <>
+                    <NavLink to={'/login'}><a className="btn min-h-0 h-8 bg-transparent hover:bg-[#DEF2F1] font-bold rounded px-4 border-2 border-[#FEFFFF] text-[#FEFFFF] hover:text-black hover:border-[#FEFFFF]">Login</a></NavLink>
+                </>}
             </div>
+            <ToastContainer />
         </div>
     );
 };
